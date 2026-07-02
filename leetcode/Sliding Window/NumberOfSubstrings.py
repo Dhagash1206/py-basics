ffaaeb599edb572fb2be966c650db84f6e9db0be
Dhagash pandya <dhagashpandya2@gmail.com>
2026-07-02 23:56:55 +0400

    Added NumberOfSubstrings.py

---
+++ leetcode/Sliding Window/NumberOfSubstrings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+
+        count = {'a': 0, 'b': 0, 'c': 0}
+        l = 0
+        ans = 0
+        n = len(s)
+
+        for r in range(n):
+
+            count[s[r]] += 1
+
+            while count['a'] > 0 and count['b'] > 0 and count['c'] > 0:
+
+                ans += (n - r)
+
+                count[s[l]] -= 1
+                l += 1
+
+        return ans
+
+        
