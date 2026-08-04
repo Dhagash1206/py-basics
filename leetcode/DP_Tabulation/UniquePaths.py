facaa698fcaf00db05e9aacefcf32e1101c67cc0
Dhagash <dhagashpandya2@gmail.com>
2026-08-04 18:13:16 +0400

    Added UniquePaths.py

---
+++ leetcode/DP_Tabulation/UniquePaths.py
@@ -0,0 +1,34 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        dp = [1] * n
+
+        for _ in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j - 1]
+
+        return dp[-1]
+
+
+# 2nd method
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        dp = [[0] * n for _ in range(m)]
+
+        # First row
+        for j in range(n):
+            dp[0][j] = 1
+
+        # First column
+        for i in range(m):
+            dp[i][0] = 1
+
+        # Fill remaining cells
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[m - 1][n - 1]
